9c6475568dea0fa65ec1eeb4543f8299d9c5b7a1
chejianchao <820992376@qq.com>
2018-07-23 15:06:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student_hash|
+      Student.new(student_hash)
+    }
   end
 
   def add_student_attributes(attributes_hash)
@@ -20,7 +22,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
