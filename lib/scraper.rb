3beedc231ca258e2e7b551cb829bfd8225bf4ed7
chejianchao <820992376@qq.com>
2018-07-23 14:18:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'open-uri'
 require 'pry'
-
+require "Nokogiri"
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri.HTML(open(index_url))
+    doc
   end
 
   def self.scrape_profile_page(profile_url)
