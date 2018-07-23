d1e347c2143601216e0c1444afb213eccbe867ab
chejianchao <820992376@qq.com>
2018-07-23 14:20:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri.HTML(open(index_url))
-    doc
+    doc.css("roster-cards-container").size
   end
 
   def self.scrape_profile_page(profile_url)
