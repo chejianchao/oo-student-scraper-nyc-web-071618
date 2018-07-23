74be25701cecd2e977382ebed3f13108e4d7330c
chejianchao <820992376@qq.com>
2018-07-23 15:00:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
     profile = {}
     doc = Nokogiri.HTML(open(profile_url))
     social_node = doc.css(".social-icon-container a")
-    keys = [:twitter, :linkedin, :github]
+    #keys = [:twitter, :linkedin, :github]
     
     social_node.each_with_index {|s, index|
       href = s.attribute("href").value
