55efac940f77d56634906a301235f483581ed104
chejianchao <820992376@qq.com>
2018-07-23 14:28:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
       name = div.css(".card-text-container h4").text
       location = div.css(".card-text-container p").text
       #{ :name => name, :location => location, :profile_url => profile_url}
-      
+      puts name
     }
   end
 
