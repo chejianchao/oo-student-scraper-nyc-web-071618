26fb47cebe5c6cbf2a6c6c9499b3faddb02b2727
chejianchao <820992376@qq.com>
2018-07-23 14:47:20 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
     }
     quote = doc.css(".vitals-text-container div").first.text
     profile[:profile_quote] = '"'+ quote + '"'
-    bio = doc.css(".details-container .bio-block details-block .description-holder p").text
+    bio = doc.css(".details-container .bio-block .details-block .description-holder p").text
     puts bio`
   end
 
